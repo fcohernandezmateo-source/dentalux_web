324ee4b2608dcc7442d637cc34f1705ef3c33a9a
Francisco Hernández <124732613+fran-hernandez@users.noreply.github.com>
2026-02-22 19:04:30 +0100

    Uso de onboarding email para test final

--- api/contacto.js
+++ api/contacto.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   try {
     await resend.emails.send({
-      from: 'DentalLux <info@send.dentalux.es>',
+      from: 'DentalLux <onboarding@resend.dev>',
       to: ['info@dentalux.es'],
       subject: `Consulta Web: ${service} - ${name}`,
       html: `
